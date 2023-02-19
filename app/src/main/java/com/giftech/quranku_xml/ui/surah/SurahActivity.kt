f108f02e8988bdb815216d9b4981109f1778cd90
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-02-19 17:54:18 +0700

    create SurahActivity.kt

---
+++ app/src/main/java/com/giftech/quranku_xml/ui/surah/SurahActivity.kt
@@ -0,0 +1,12 @@
+package com.giftech.quranku_xml.ui.surah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.giftech.quranku_xml.R
+
+class SurahActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_surah)
+    }
+}
