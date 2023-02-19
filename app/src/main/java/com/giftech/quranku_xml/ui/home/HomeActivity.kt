62d29abfdb9e4112e1d0bd325d1c54f6287db706
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-02-19 17:54:50 +0700

    add intent to surah

--- app/src/main/java/com/giftech/quranku_xml/ui/home/HomeActivity.kt
+++ app/src/main/java/com/giftech/quranku_xml/ui/home/HomeActivity.kt
@@ -1,11 +1,13 @@
 package com.giftech.quranku_xml.ui.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import com.giftech.quranku_xml.R
 import com.giftech.quranku_xml.databinding.ActivityHomeBinding
+import com.giftech.quranku_xml.ui.surah.SurahActivity
 import com.giftech.quranku_xml.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,7 +20,11 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        surahAdapter = SurahAdapter()
+        surahAdapter = SurahAdapter{ nomorSurah ->
+            val intent = Intent(this, SurahActivity::class.java)
+            intent.putExtra("nomorSurah", nomorSurah)
+            startActivity(intent)
+        }
         binding.rvSurah.adapter = surahAdapter
 
         viewModel.listSurah.observe(this){
